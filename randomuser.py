f0017256d30558ee97e7b81f0450ec59deb01f82
khakimovSamdu <xakimov2806@gmail.com>
2024-07-16 18:47:41 -0800

    randomuser all

--- randomuser.py
+++ randomuser.py
@@ -2,78 +2,82 @@ import requests
 URL = 'https://randomuser.me/api/'
 class RandomUser:
     def __init__(self):
-        self.data = requests.get(URL).json()
-
+        self.data = requests.get(URL).json()['results'][0]
 
     def get_first_name(self):
         """
         This method returns the first name
         """
-        return self.data['results'][0]['name']['first']
+        return self.data['name']['first']
     
     def get_last_name(self):
         """
         This method returns the last name
         """
-        pass 
+        return self.data['name']['last'] 
 
     def get_full_name(self):
         """
         This method returns the full name
         """
-        pass 
+        return self.data['name']['first'] + ' ' + self.data['name']['last'] 
 
     def get_email(self):
         """
         This method returns the email
         """
-        pass 
+        return self.data['email'] 
 
     def get_phone(self):
         """
         This method returns the phone
         """
-        pass 
+        return self.data['phone'] 
 
     def get_cell(self):
         """
         This method returns the cell
         """
-        pass 
+        return self.data['call']  
 
     def get_picture(self):
         """
         This method returns the picture
         """
-        pass
+        return self.data['picture']['medium']
     def get_gender(self):
         """
         This method returns the gender
         """
-        pass
+        return self.data['gender'] 
     def get_nationality(self):
         """
         This method returns the nationality
         """
-        pass
+        return self.data['nat'] 
     def get_location(self):
         """
         This method returns the location
         """
-        pass
+        location = {
+            'latitude': self.data['location']['coordinates']['latitude'],
+            'longitude': self.data['location']['coordinates']['longitude']
+
+        }
+        return location
     def get_date(self):
         """
         This method returns the date
         """
-        pass
+        return self.data['dob']['date']
     def get_dob(self):
         """
         This method returns the dob
         """
-        pass
+        return self.data['dob']
 
     def get_age(self):
         """
         This method returns the age
         """
-        pass
+        return self.data['dob']['age']
